3b85cabcb6f4f009975f2cb2cb6ca89079b32588
isaacross <Theisaacross@gmail.com>
2021-12-07 15:09:34 -0700

    working get and post routes + whitelisting

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ from resources.stats import stats
 #this statement will import all variables and methods/functions from a file as properties on the model object
 import models
 
+#import our package for handling cors
+from flask_cors import CORS
+#^^ our origin is 8000 and react's origin is 3000 so we need to whitelist 3000 so our frontend can make requests
+
 DEBUG=True
 
 PORT=8000
 
 app = Flask(__name__) #like const app = express()
+
+#whitelisting
+CORS(stats, origin=['http://localhost:3000'], supports_credentials=True) #support allows cookies and sessions for auth
 
 #use this blueprint( component/controller of the app) to handle anything related to stats
 app.register_blueprint(stats, url_prefix='/api/v1/stats')
--- resources/stats.py
+++ resources/stats.py
@@ -3,12 +3,46 @@ import models
 # a blueprint is a way to create a self-contained grouping of related functionalities in an app
 # we are going to use a blueprint as a controller
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify #request is basically req.body
+
+# useful tools from peewee
+from playhouse.shortcuts import model_to_dict
 
 #first argument is blueprint's name
 #second argument is the import_name
 stats = Blueprint('stats', 'stats')
 
-@stats.route('/')
+@stats.route('/', methods=['GET'])
 def index():
-    return "index route is working"
+    result = models.Score.select()
+    print(result) #looks like SQL
+
+    #use a loop to populate all models to dictionaries
+    score_dicts = []
+    for score in result:
+        score_dict = model_to_dict(score)
+        score_dicts.append(score_dict)
+        
+    return jsonify(
+        data=score_dicts,
+        message=f"Successfully found {len(score_dicts)} scores",
+        status=200
+    ),200
+
+@stats.route('/', methods=['POST'])
+def create_score():
+    # .get_json() attached to the request will extract JSON from the request body. (like req.body)
+    payload = request.get_json()
+    print(payload) #shows the request
+
+    new_score = models.Score.create(date=payload['date'], location=payload['location'], hole=payload['hole'], score=payload['score'], putts=payload['putts'])
+    #^ this creates a new model with our schema
+
+    score_dict = model_to_dict(new_score)
+    #^ converts model to dictionary
+
+    return jsonify(
+        data=score_dict,
+        message='Successfully created score',
+        status=201
+    ), 201
